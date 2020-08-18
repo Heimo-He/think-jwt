ad2350b2b55531ec6429254083c2664c3371ce04
Heimo <heimo9397@gmail.com>
2020-08-18 18:22:05 +0800

    update config and readme

--- README.md
+++ README.md
@@ -1,2 +1,37 @@
 # think-jwt
 基于thinkphp6的JWT权限验证插件
+
+### 支持特性
+- hash对称加密
+- OpenSSL非对称加密
+- 自动刷新token
+- header或body获取
+
+## Example
+
+### 配置文件`config/jwt.php`，公私玥使用openssl生成
+
+```shell
+$ openssl
+$ OpenSSL> genrsa -out rsa_private_key.pem 1024
+$ OpenSSL> rsa -in rsa_private_key.pem -pubout -out rsa_public_key.pem
+```
+
+### 应用中`middleware.php`注册jwt校验中间件
+
+```php
+<?php
+// 全局中间件定义文件
+return [
+
+    ...
+
+    \heimo\auth\middleware\Jwt::class //增加这行
+];
+```
+
+### 生成token
+
+```php
+Auth::make()->generateToken();
+```
--- src/config/jwt.php
+++ src/config/jwt.php
@@ -2,27 +2,30 @@
 return [
 
     //加密密钥
-    'secret' => MD5('secret'),
+    'secret'      => MD5('secret'),
 
-    //加密密钥
-    'private_key' => '',
+    //加密私钥
+    'private_key' => <<<EOF
 
-    //加密密钥
-    'public_key' => '',
+EOF,
 
+    //解密私钥
+    'public_key'  => <<<EOF
+
+EOF,
     //加密的有效期(s)
-    'ttl' => 180,
+    'ttl'         => 60,
 
     //过期多久以内允许刷新(s)
-    'refresh_ttl' => 360,
+    'refresh_ttl' => 60,
 
     //传输方式(header/body)
-    'type' => 'header',
+    'type'        => 'header',
 
     //密文传输的字段名称
-    'key' => 'Authorization',
+    'key'         => 'Authorization',
 
     //加密方式 'HS256','HS384','HS512','RS256','RS384','RS512'
-    'alg' => 'RS256'
+    'alg'         => 'RS256'
 
 ];
